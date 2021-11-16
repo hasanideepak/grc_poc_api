7e4b4955e0581cbe047a36732ea42c493bb63317
Deepak <deepak.hasani@algoventures.com>
2021-11-16 18:15:40 +0530

    changed env file

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,6 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/check_api', async (req,res) => {
-    res.send('Hello new world !')
+    res.send('Hello Docker !')
 })
 export default router;
