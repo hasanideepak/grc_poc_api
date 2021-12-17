05e7fc467528a727c61d2a189423209a272490fb
Deepak <deepak.hasani@algoventures.com>
2021-12-17 18:40:01 +0530

    changed to check pipeline

--- utils/pg_helper.js
+++ utils/pg_helper.js
@@ -6,9 +6,9 @@ export const selectSql = async (sql) => {
             //console.log(sql);
             postgress.query(sql, function (error, results) {
                 if (error) {
-                    resolve({ status_code: 'grc404', message: 'Error in query' });
+                    resolve({ status_code: 'air404', message: 'Error in query' });
                 } else {
-                    resolve({ status_code: 'grc200', message: 'Success', results: results.rows });
+                    resolve({ status_code: 'air200', message: 'Success', results: results.rows });
                 }
             });
         } catch (error) {
