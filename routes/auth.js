114d9a640ddb920f3fa8b3f3e142d78dbec42b51
Deepak <deepak.hasani@algoventures.com>
2022-01-14 15:51:42 +0530

    added org_id and access_role in login api

--- routes/auth.js
+++ routes/auth.js
@@ -8,8 +8,8 @@ router.post('/login', async (req, res) => {
     const { email, password } = req.body;
     if (email != undefined) {
         if (password != undefined || password != '' || password != null) {
-            let sql = `select a.user_id,a.email,a.org_emp_id,b.name,b.phone,c.name as org_name,c.logo,case coalesce(d.account_id,0) when 0 then 'N' else 'Y' end as is_onboard from master.users a,master.org_employees b,master.orgs c left join master.accounts d on c.org_id = d.org_id 
-                       where a.username = '${email}' and a.password = '${password}' and a.org_emp_id = b.emp_id and b.org_id = c.org_id`;
+            let sql = `select a.user_id,a.email,a.org_emp_id,b.name,b.phone,c.name as org_name,c.logo,c.org_id,case coalesce(d.account_id,0) when 0 then 'N' else 'Y' end as is_onboard,dept.name as access_role from master.users a,master.org_employees b,reference.departments dept,master.x_org_dept_emp org_dept,master.orgs c left join master.accounts d on c.org_id = d.org_id 
+            where a.username = '${email}' and a.password = '${password}' and a.org_emp_id = b.emp_id and b.org_id = c.org_id and b.emp_id = org_dept.emp_id and org_dept.dept_id = dept.id`;
             let resp = await selectSql(sql);
             if (resp.results.length > 0) {
                 let result = resp.results[0];
